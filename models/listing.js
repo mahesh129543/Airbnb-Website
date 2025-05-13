3173de8ded91fae95b1cc6adc031c008ccef28fb
mahi <mahi@adf.in>
2025-05-14 00:57:34 +0530

    changes

--- models/listing.js
+++ models/listing.js
@@ -11,7 +11,7 @@ const listingSchema = new Schema({
         filename: String,
         url:String,
        
-        //set:(v) => v==="" ? "https://in.images.search.yahoo.com/search/images;_ylt=AwrKD20VURtohwIAibu7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=image+unsplash&fr2=piv-web&type=E210IN885G0&fr=mcafee#id=0&iurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1562170824-b547dae88b97%3Fixlib%3Drb-1.2.1%26q%3D80%26fm%3Djpg%26crop%3Dentropy%26cs%3Dtinysrgb%26w%3D1080%26fit%3Dmax%26ixid%3DeyJhcHBfaWQiOjEyMDd9&action=click" : v,
+set:(v) => v==="" ? "https://in.images.search.yahoo.com/search/images;_ylt=AwrKD20VURtohwIAibu7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=image+unsplash&fr2=piv-web&type=E210IN885G0&fr=mcafee#id=0&iurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1562170824-b547dae88b97%3Fixlib%3Drb-1.2.1%26q%3D80%26fm%3Djpg%26crop%3Dentropy%26cs%3Dtinysrgb%26w%3D1080%26fit%3Dmax%26ixid%3DeyJhcHBfaWQiOjEyMDd9&action=click" : v,
     }],
     price: Number,
     location: String,
